10a5fe06ac433507418397570582b209bdfecb11
Liao Xueqing <xueqingliao@foxmail.com>
2020-08-22 16:45:15 +0800

    Add mouse move event stream

--- src/app/stream/stream.component.ts
+++ src/app/stream/stream.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
+import { flatMap, takeUntil, map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-stream',
   templateUrl: './stream.component.html',
   styleUrls: ['./stream.component.less']
 })
-export class StreamComponent implements OnInit {
+export class StreamComponent implements OnInit, OnDestroy {
 
+  subscription: Subscription;
   constructor() { }
 
   ngOnInit(): void {
+    const el = document.getElementById('my-element');
+
+    const mouseMoves = fromEvent(document, 'mousemove');
+
+    this.subscription = mouseMoves.subscribe((evt: MouseEvent) => {
+      
+      el.innerHTML = `Coords: ${evt.clientX} X ${evt.clientY}`;
+
+      if (evt.clientX < 40 && evt.clientY < 40) {
+        this.subscription.unsubscribe();
+      }
+    });
+  };
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
