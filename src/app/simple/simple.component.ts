742aa3934ce76d4914525e77fb5e1c2a2c2dfb12
Liao Xueqing <xueqingliao@foxmail.com>
2020-08-22 11:41:41 +0800

    Add simple demo

--- src/app/simple/simple.component.ts
+++ src/app/simple/simple.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { fromEvent } from 'rxjs';
 @Component({
   selector: 'app-simple',
   templateUrl: './simple.component.html',
   styleUrls: ['./simple.component.less']
 })
 export class SimpleComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    const el = document.getElementById('my-element');
 
+    const mouseMoves = fromEvent(document, 'mousemove');
+
+    const subscription = mouseMoves.subscribe((evt: MouseEvent) => {
+      
+      el.innerHTML = `Coords: ${evt.clientX} X ${evt.clientY}`;
+
+      if (evt.clientX < 40 && evt.clientY < 40) {
+        subscription.unsubscribe();
+      }
+    });
+  }
 }
